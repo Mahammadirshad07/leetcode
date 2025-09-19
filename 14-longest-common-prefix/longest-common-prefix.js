ce851ed8a6469cb19a7c08e5556e336cc18dc5d9
MAHAMMAD IRSHAD <157797281+Mahammadirshad07@users.noreply.github.com>
2025-09-19 18:29:29 +0530

    Time: 0 ms (100.00%) | Memory: 53.9 MB (74.03%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -5,11 +5,11 @@
 var longestCommonPrefix = function(strs) {
       if (!strs.length) return "";
 
-  let prefix = strs[0]; // assume first word is prefix
+  let prefix = strs[0]; 
 
   for (let i = 1; i < strs.length; i++) {
     while (strs[i].indexOf(prefix) !== 0) {
-      prefix = prefix.slice(0, -1); // shrink prefix
+      prefix = prefix.slice(0, -1); 
       if (prefix === "") return "";
     }
   }
