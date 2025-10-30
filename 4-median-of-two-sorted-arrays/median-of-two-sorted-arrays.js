131d36c7d15034f4101271993f82f0adeed4f864
MAHAMMAD IRSHAD <157797281+Mahammadirshad07@users.noreply.github.com>
2025-10-30 10:27:52 +0530

    Time: 2 ms (88.77%) | Memory: 57.9 MB (83.06%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let merged = [];
+  let i = 0, j = 0;
+
+  while (i < nums1.length && j < nums2.length) {
+    if (nums1[i] < nums2[j]) merged.push(nums1[i++]);
+    else merged.push(nums2[j++]);
+  }
+
+  while (i < nums1.length) merged.push(nums1[i++]);
+  while (j < nums2.length) merged.push(nums2[j++]);
+
+  const n = merged.length;
+  if (n % 2 === 1) return merged[Math.floor(n / 2)];
+  else return (merged[n / 2 - 1] + merged[n / 2]) / 2; 
+    
+};
