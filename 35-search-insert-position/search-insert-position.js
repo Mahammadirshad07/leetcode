ef9ca1fb9c354c2530d1c74da0c3d8c30553a80e
MAHAMMAD IRSHAD <157797281+Mahammadirshad07@users.noreply.github.com>
2025-11-26 15:53:25 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (73.86%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function searchInsert(nums, target) {
+  let l = 0, r = nums.length - 1
+  while (l <= r) {
+    let mid = Math.floor((l + r) / 2)
+    if (nums[mid] === target) return mid
+    if (nums[mid] < target) l = mid + 1
+    else r = mid - 1
+  }
+  return l
+}
